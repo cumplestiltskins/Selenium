5e6a31926077411b4a6edbf41d08603339f797ee
cumplestiltskins <muditmeshram47@gmail.com>
2025-02-12 13:03:32 +0530

    Update TypeRacer.py
    text done (180+ wps) captcha remaining

--- TypeRacer.py
+++ TypeRacer.py
@@ -7,58 +7,58 @@ import pyautogui
 
 class PythonOrgSearch(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Edge()
+	def setUp(self):
+		self.driver = webdriver.Edge()
 
-    def functionality(self, words):
-        wordList = []
+	def functionality(self, words):
+		wordList = []
 
-        for i in words:
-            wordList.append(i.text)
-        result = ''.join(wordList)
-        return result
-    
-    def checkComma(self, span):
-        for i in span:
-            if ',' in i.text:
-                return ','
-        return ''
+		for i in words:
+			wordList.append(i.text)
+		result = ''.join(wordList)
+		return result
+	
+	def checkComma(self, span):
+		for i in span:
+			if ',' in i.text:
+				return ','
+		return ''
 
-        
+		
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("https://play.typeracer.com/")
-        self.assertIn("TypeRacer", driver.title)
-        time.sleep(2)
-        but = (driver.find_element(By.ID, "gwt-uid-1")).find_element(By.CLASS_NAME, "prompt-button")
-        but.click()
+	def test_search_in_python_org(self):
+		driver = self.driver
+		driver.get("https://play.typeracer.com/")
+		self.assertIn("TypeRacer", driver.title)
+		time.sleep(2)
+		but = (driver.find_element(By.ID, "gwt-uid-1")).find_element(By.CLASS_NAME, "prompt-button")
+		but.click()
 
-        time.sleep(2)
+		time.sleep(2)
 
-        main_div = input("enter main div: ")
-        
-        div = driver.find_element(By.CLASS_NAME, main_div)
-        
+		span = driver.find_elements(By.TAG_NAME, "span")
+		for i in span:
+			if len(i.text) >100 :
+				words = i.text 
+		#main_div = input("enter main div: ")
+		#
+		#div = driver.find_element(By.CLASS_NAME, main_div)y - no
+		
+		wp = driver.find_element(By.CLASS_NAME, "txtInput")
+		ss = input()
+		time.sleep(2)
+		
 
-        word_class = input("enter word class: ")
-
-        time.sleep(5)
-        words = driver.find_elements(By.CLASS_NAME, word_class)
-        
-        for i in range(100):
-            
-            span = div.find_elements(By.TAG_NAME, "span")
-            words = driver.find_elements(By.CLASS_NAME, word_class)
-            pyautogui.write(f"{self.functionality(words)}{self.checkComma(span)} ", interval=0)
-
-
-        self.assertNotIn("No results found.", driver.page_source)
+		for i in words:
+			wp.send_keys(i)
 
 
-    def tearDown(self):
-        time.sleep(5)
-        self.driver.close()
+		self.assertNotIn("No results found.", driver.page_source)
+
+
+	def tearDown(self):
+		time.sleep(100)
+		self.driver.close()
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
